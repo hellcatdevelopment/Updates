34a9f4798b24743c81a61a3e920ad3d6c23329eb
chawklet <115399584+hellcatdevelopment@users.noreply.github.com>
2023-09-07 19:15:05 +0200

    Add files via upload

---
+++ UPDATES/main/up.js
@@ -0,0 +1,14 @@
+{
+	"556_alchemy": "0.1",
+	"556_raids": "0.1",
+	"556_notepad": "1.0",
+	"556_robbery": "1.0",
+	"556_guarma": "1.0",
+	"556_customspawn": "1.0",
+	"556_lootchest": "1.0",
+	"556_mining": "1.3",
+	"556_lumberjack": "1.3",
+	"556_trainrob": "0.1",
+	"hc_fishing": "0.1",
+	"hc_exchange": "0.1",
+}
